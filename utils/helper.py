37cc32ffaf978440ce4750bdb552a97415a121e6
YuboZhang <cscrypto@163.com>
2020-08-13 14:35:36 +0800

    add proxy

--- utils/helper.py
+++ utils/helper.py
@@ -6,6 +6,11 @@ __maintainer__ = "Chirag Rathod (Srce Cde)"
 
 import requests
 
+proxies = {
+  "http": "socks5://127.0.0.1:1080",
+  "https": "socks5://127.0.0.1:1080",
+}
+
 def openURL(URL, params):
-    r = requests.get(URL + "?", params=params)
+    r = requests.get(URL + "?", params=params, proxies=proxies)
     return r.text
